9dfefe17c30f24236d02f4293ddce3b062faa862
cocoli <hanstar1127@gmail.com>
2015-10-12 13:04:28 -0300

    Apostila de MongoDb

--- sistema-mongoose-atomico-promises-events/server/modules/users/actions/action.retrieve.js
+++ sistema-mongoose-atomico-promises-events/server/modules/users/actions/action.retrieve.js
@@ -1,20 +1,6 @@
-'use strict';
-
-let user = require('../user_model');
-
 const Action = {
   retrieve: function(req, res) {
-	
-		let promise = user.find({}).exec();
-		promise.then(succesHandler , errorHandler)
-
-		function succesHandler (users){
-			res.status(200).json(users);
-		}
-
-		function errorHandler (err) {
-			res.status(400).json(err);
-		}
+    res.json({action: 'retrieve'});
   }
 }
 module.exports = Action;
--- sistema-mongoose-atomico-promises-events/server/tests/users-routes.test.js
+++ sistema-mongoose-atomico-promises-events/server/tests/users-routes.test.js
@@ -6,64 +6,26 @@ let request =  require('superagent');
 let url = require('url');
 const ABS_URL = "http://localhost:3000/api/users";
 
-describe('testing of api/users routes',() => {
+describe('testing of users routes',() => {
 	let id = null;
 	let user = {
 		name : ch.Name.firstName(),
 		email : ch.Internet.email(ch.Name.name()),
-		password : ch.Number.number(8)
+		password : ch.Number.number(6)
 	};
-
+	
 	it('should be create one user', done => {
 		 request
       .post(url.resolve(ABS_URL,'users'))
       .send(user)
       .end((err , res) => {
         res.should.be.exist;
+        res.body.should.be.a('Object');
         res.body._id.should.exist;
-        id = res.body._id;
         res.status.should.be.eql(201);
+        id = res.body._id;
         done();
       });	
 	});
-	
-	it('should be get retrive all users', done => {
-		 request
-      .get(url.resolve(ABS_URL,'users'))
-			.end((err, res) => {
-				res.should.be.exist;	
-        res.status.should.be.eql(200);
-				res.body.should.be.an("Array");
-				done();
-			});
-	});
-
-	it('should be update user by id', done => {
-		let user = {
-			name : ch.Name.firstName(),
-			email : ch.Internet.email(ch.Name.name()),
-			password : ch.Number.number(8)
-		};
-
-		request
-      .put(url.resolve(ABS_URL,'users/'+id))
-			.send(user)
-			.end((err, res) => {
-				res.should.be.exist;	
-        res.status.should.be.eql(202);
-				done();
-			});
-	});
-
-	it('should be delete user by id', done => {
-		request
-      .del(url.resolve(ABS_URL,'users/'+id))
-			.end((err, res) => {
-				res.should.be.exist;	
-        res.status.should.be.eql(204);
-				done();
-			});
-	});
-
 
 });
