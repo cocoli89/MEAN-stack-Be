ca5cb39c6fd3d0a3e3feac51ccc3674d94449ae4
cocoli <hanstar1127@gmail.com>
2016-01-15 11:56:04 -0200

    atualizando as fitas

--- be-mean-code-examples/filesystem/1writefile.js
+++ be-mean-code-examples/filesystem/1writefile.js
@@ -17,5 +17,5 @@ fs.writeFile("./hello2.txt","hello, I'm a motherfucker async content", function 
   console.log(result);
 });
 
-var writeStream = fs.createWriteStream('./helloStream.txt', {defaultEnconding:'utf8'});
+var writeStream = fs.createWriteStream('./helloStream.txt', {defaultEnconding:'utf8k'});
 writeStream.write("Hello,  I'm a writable stream!");
